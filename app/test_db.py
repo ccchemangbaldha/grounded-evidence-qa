15d2aa8d880d29764df49dc43edc799d12369090
Hemang Baldha <ccc.hemangbaldha@gmail.com>
2026-01-06 14:32:39 +0530

    added db files and done with databse testing

--- app/test_db.py
+++ app/test_db.py
@@ -1,17 +1,8 @@
 from app.db import get_connection
 
-def main():
-    conn = get_connection()
-    cursor = conn.cursor()
+conn = get_connection()
+cursor = conn.cursor()
 
-    cursor.execute(
-        "INSERT INTO documents (name) VALUES (?)",
-        ("sample.txt",)
-    )
-
-    conn.commit()
-    conn.close()
-    print("Document inserted successfully")
-
-if __name__ == "__main__":
-    main()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+print(cursor.fetchall())
+conn.close()
