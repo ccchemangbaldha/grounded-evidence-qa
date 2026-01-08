6c0e70e7729287ac6fa285f6e33978b84ccc5ecb
Hemang Baldha <ccc.hemangbaldha@gmail.com>
2026-01-08 10:44:37 +0530

    bind UI with fastAPI

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI
-from app.api.routes import router
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from pathlib import Path
 
+from app.api.routes import router 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def serve_index():
+    index_path = Path("static/index.html")
+    return index_path.read_text(encoding="utf-8")
 
 app.include_router(router)
